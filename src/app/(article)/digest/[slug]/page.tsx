3232365c614ab9dd9c3af001db9f01cf114ed9c3
Lanqing Huang <lqhuang@outlook.com>
2024-06-03 23:25:31 +0800

    feat: Add title and meta components

--- src/app/(article)/digest/[slug]/page.tsx
+++ src/app/(article)/digest/[slug]/page.tsx
@@ -2,6 +2,9 @@ import type { Source } from 'src/content/local'
 
 import { join } from 'path'
 
+import Head from 'next/head'
+
+import { Meta } from 'src/components/meta'
 import { Markdown } from 'src/components/markdown'
 import { buildLocalSource } from 'src/content/local'
 import { contentDir } from 'src/config'
@@ -18,9 +21,21 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const db = await buildLocalSource(join(contentDir, 'snapshots'))
   const source = db.get(slug) as Source
 
+  const { frontmatter, date } = source
+
+  const title = frontmatter.title ? frontmatter.title : slug
+
+  const { created, updated, tags } = frontmatter
+
   return (
     <>
       <article>
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <h1> {title} </h1>
+        <Meta date={date} created={created} updated={updated} tags={tags} />
+        ---
         <Markdown content={source.content} />
       </article>
     </>

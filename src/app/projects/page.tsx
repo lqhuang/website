9f32718223769f45bd1ac48ddd73c323879a1008
Lanqing Huang <lqhuang@outlook.com>
2025-02-25 22:17:38 +0800

    bugfix: Fix eslint error

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,13 +6,13 @@ import { allProjects } from 'src/content/projects'
 export const dynamicParams = false
 export const dynamic = 'force-static'
 
-export default async function Page({}) {
+export default function Page({}) {
   const post = allProjects[0]
 
   return (
     <WellTyped key={post.metadata.slug}>
       <h1 className="no-underline hover:underline">
-        The Projects I'm Working on
+        The Projects I{"'"}m Working on
       </h1>
       <Markdown content={post.content} />
     </WellTyped>

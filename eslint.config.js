afb70d1c5da7259ede3f85ba489947524cfb8a39
Lanqing Huang <lqhuang@outlook.com>
2025-05-01 22:11:37 +0800

    chore: Rename config file for toolchains

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,21 @@
 // @ts-check
-
+import { FlatCompat } from '@eslint/eslintrc'
 import tseslint from 'typescript-eslint'
 import react from '@eslint-react/eslint-plugin'
-
-import { FlatCompat } from '@eslint/eslintrc'
 import * as eslintConfigPrettier from 'eslint-config-prettier'
 import * as eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname })
 
 export default tseslint.config([
-  react.configs.recommended,
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  react.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: { parserOptions: { project: true } },
-  },
   eslintPluginPrettierRecommended,
   eslintConfigPrettier, // eslint-config-prettier last
+  { languageOptions: { parserOptions: { project: true } } },
   {
     rules: {
       '@typescript-eslint/require-await': 'warn',
